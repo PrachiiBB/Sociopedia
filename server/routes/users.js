b54b1e9312b66a829e9d38bb34895e0d5089dee6
Prachi Balla <prachi.balla@aissmsioit.org>
2023-05-23 16:30:31 +0530

    First Commit

---
+++ server/routes/users.js
@@ -0,0 +1,20 @@
+import  Express  from "express";
+import {
+    getUser,
+    getUserFriends,
+    addRemoveFriend,
+} from "../controllers/user.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = Express.Router();
+
+// READ 
+
+router.get("/:id", verifyToken,getUser);
+router.get("/:id/friends",verifyToken,getUserFriends);
+
+//UPDATE
+
+router.patch("/:id/:friendId", verifyToken,addRemoveFriend);
+
+export default router;
